8adc91a0f69448438c3529417c6a5af056d02979
iddrader <iddrader59@gmail.com>
2024-08-02 22:15:35 +0300

    Create user info, chats list, add Inter font

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -1,7 +1,55 @@
+import { useState } from "react";
 import "../assets/scss/chatList.scss";
 
 const ChatList = () => {
-    return <div className="chatList">Chat list</div>;
+    const [addMode, setAddMode] = useState(false);
+
+    return (
+        <div className="chatList">
+            <div className="search">
+                <div className="searchBar">
+                    <img src="/search.png" alt="" />
+                    <input type="text" placeholder="Search" />
+                </div>
+                <img
+                    src={addMode ? "minus.png" : "/plus.png"}
+                    alt=""
+                    className="add"
+                    onClick={() =>
+                        setAddMode((prevState: boolean) => !prevState)
+                    }
+                />
+            </div>
+            <div className="item">
+                <img src="/avatar.png" alt="" />
+                <div className="textContainer">
+                    <span>Jane Doe</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="/avatar.png" alt="" />
+                <div className="textContainer">
+                    <span>Jane Doe</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="/avatar.png" alt="" />
+                <div className="textContainer">
+                    <span>Jane Doe</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="/avatar.png" alt="" />
+                <div className="textContainer">
+                    <span>Jane Doe</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ChatList;
