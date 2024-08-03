46716f74d28ca0712b4f9169d9202ddfbe878188
iddrader <iddrader59@gmail.com>
2024-08-03 12:35:05 +0300

    Create add user modal

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Login from "./components/Login";
 import Notification from "./components/Notification";
 
 function App() {
-    const user = false;
+    const user = true;
 
     return (
         <div className="container">
--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "../assets/scss/chatList.scss";
+import AddUser from "./AddUser";
 
 const ChatList = () => {
     const [addMode, setAddMode] = useState(false);
@@ -46,6 +47,7 @@ const ChatList = () => {
                     <p>Hello</p>
                 </div>
             </div>
+            {addMode && <AddUser />}
         </div>
     );
 };
